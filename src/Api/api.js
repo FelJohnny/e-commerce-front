7fcf9047e73fbbeb867fa7606e1f2ee83101fe4e
Felipe Johnny <felipijohnny@outlook.com>
2024-06-09 12:59:11 -0300

    ajustando url de consumo da API

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,5 +1,5 @@
 // const URL = "http://localhost:3333";
-const URL = "http://54.232.171.58:3333";
+const URL = "https://54.232.171.58";
 //=================LOGIN/REGISTER====================//
 
 export function POST_LOGIN(dataLogin) {
